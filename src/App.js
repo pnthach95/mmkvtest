e418519335d2b707b5e4568cd882700edf62a79d
Pham Ngoc Thach <thachxyz123@gmail.com>
2021-09-21 23:39:20 +0700

    Add set empty string, set 0

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import useStorage from './useMMKV';
 
 const App = () => {
   const [bool, setBool] = useStorage('bool');
+  const [str, setStr] = useStorage('string');
+  const [num, setNum] = useStorage('num');
 
   const onPressFalse = () => {
     setBool(false);
@@ -14,12 +16,40 @@ const App = () => {
     setBool(true);
   }
 
+  const onPressEmptyString = () => {
+    setStr('');
+  }
+
+  const onPressString = () => {
+    setStr('this is a string');
+  }
+
+  const onPress0 = () => {
+    setNum(0)
+  }
+
+  const onPress1 = () => {
+    setNum(1)
+  }
+
   return <View style={styles.container}>
     <Text>{bool ? 'True' : 'False'}</Text>
     <View style={styles.space}/>
     <Button title='Set false' onPress={onPressFalse} />
     <View style={styles.space}/>
     <Button title='Set true' onPress={onPressTrue} />
+    <View style={styles.divider} />
+    <Text>{str}</Text>
+    <View style={styles.space}/>
+    <Button title='Set empty string' onPress={onPressEmptyString} />
+    <View style={styles.space}/>
+    <Button title='Set string' onPress={onPressString} />
+    <View style={styles.divider} />
+    <Text>{num}</Text>
+    <View style={styles.space}/>
+    <Button title='Set 0' onPress={onPress0} />
+    <View style={styles.space}/>
+    <Button title='Set 1' onPress={onPress1} />
   </View>;
 };
 
@@ -29,6 +59,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 10,
+  },
+  divider:{
+    backgroundColor: 'black',
+    height: 1,
+    marginVertical: 10,
   },
   space:{
     height: 10,
